bf0c9287c68d9fe086df10a45e2d76ba38a7e9fc
Bùi Dương Tín <tinbui.koks@gmail.com>
2026-03-20 15:48:02 +0700

    add src

---
+++ src/main/java/com/example/BuiDuongTin/BuiDuongTinApplication.java
@@ -0,0 +1,13 @@
+package com.example.BuiDuongTin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuiDuongTinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuiDuongTinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BuiDuongTin/BuiDuongTinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BuiDuongTin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuiDuongTinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
